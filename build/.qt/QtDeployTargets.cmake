8ed5f5f554c3bce2bd304162a37c08db0528e4ff
Celestial73 <89754624+Celestial73@users.noreply.github.com>
2025-01-22 19:13:06 +0300

    Delete build directory

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_lab3_FILE C:/Users/Michael/ROOT/MEPHI/sem3/labs/Lab3/build/lab3.exe)
-set(__QT_DEPLOY_TARGET_lab3_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_lab3_RUNTIME_DLLS C:/Mingw64/mingw64/bin/Qt6Widgets.dll;C:/Mingw64/mingw64/bin/Qt6Gui.dll;C:/Mingw64/mingw64/bin/Qt6Core.dll)
